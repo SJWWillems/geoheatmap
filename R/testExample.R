4e8bf2976bb081c6267417aec9d67c6282f77303
Maria Rados <Maria@Marias-MacBook-Air-2.local>
2024-07-08 20:07:42 +0200

    included a dummy (minimal) example to be used for StackOverflow and any other forum/messaging

--- R/testExample.R
+++ R/testExample.R
@@ -7,7 +7,7 @@ library(tidyverse)
 
 load("R/internet.rda")
 
-internet2015 <-internet%>% filter(year == 2015)
+internet2015 <-internet2015 %>% filter(year == 2015)
 internet2015 <- internet2015[-192,]# fix later, error due to not-UTF-8 encoding
 
 internet2015 <-internet2015 %>% filter(!is.na(users))
@@ -22,3 +22,30 @@ grid_data <- africa_countries_grid1
 ggplot(data = internet2015,
        mapping = aes(fill = users, facet_col = country)) +
  geom_geoheat(na.rm = TRUE)
+
+
+library(ggplot2)
+
+
+# Example dataset
+data <- data.frame(
+  country = c("USA", "Canada", "Germany", "Japan"),
+  usage = c(50, 30, 45, 60)
+)
+
+# Dummy grid_data
+grid_data <- data.frame(
+  name = c("United States", "Canada", "Germany", "Japan"),
+  code = c("US", "CA", "DE", "JP"),
+  row = c(1, 2, 3, 4),
+  col = c(1, 2, 3, 4)
+)
+
+facet_data <- data
+facet_col <- "country"
+
+# Example usage
+ggplot(data = data,
+       mapping = aes(fill = usage, facet_col = country))  +
+  geom_geoheat()
+
