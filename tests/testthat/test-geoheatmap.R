7619113f42e30ee9828052f32bca06e03cb886d6
Maria Rados <Maria@Marias-MacBook-Air-2.local>
2024-08-07 21:56:29 +0200

    preparing for release

---
+++ tests/testthat/test-geoheatmap.R
@@ -0,0 +1,16 @@
+context("basic plotting functionality")
+test_that("a plot gets produced", {
+
+  require(ggplot2)
+  require(geoheatmap)
+  require(geofacet)
+
+  data(internet)
+
+  internet_2015 <- subset(internet, year == 2015)
+  geoheatmap(facet_data = internet_2015, grid_data = europe_countries_grid1,
+             facet_col = "country", value_col = "users") -> gg
+
+  gb <- ggplot_build(gg)
+
+})
