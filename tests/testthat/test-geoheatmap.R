d628d7fdb1f43a3132c8225769f917ee0b6ce8fd
Maria Rados <Maria@Marias-MacBook-Air-2.local>
2024-08-23 14:39:26 +0200

    add expectations to test

--- tests/testthat/test-geoheatmap.R
+++ tests/testthat/test-geoheatmap.R
@@ -1,15 +1,19 @@
-test_that("a plot gets produced", {
-
+test_that("a simple plot gets produced", {
   require(ggplot2)
   require(geoheatmap)
   require(geofacet)
 
-  data(internet)
-
+  data(internet, package = "geoheatmap")
   internet_2015 <- subset(internet, year == 2015)
+
   geoheatmap(facet_data = internet_2015, grid_data = europe_countries_grid1,
-             facet_col = "country", value_col = "users") -> gg
+                           facet_col = "country", value_col = "users") -> gg
+
+  # Expect that gg is a ggplot object
+  expect_s3_class(gg, "ggplot")
 
   gb <- ggplot_build(gg)
 
+  # Expect that the plot contains at least one layer
+  expect_gt(length(gb$layout$panel_params), 0)
 })
